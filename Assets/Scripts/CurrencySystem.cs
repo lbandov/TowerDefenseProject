d2320ea162aa95cdfa99e901235345e258217b05
sbandov <stoyan.bandov@hotmail.com>
2022-06-23 18:02:29 +0300

    Currency System and Singleton and Tanks

---
+++ Assets/Scripts/CurrencySystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencySystem : Singleton<CurrencySystem>
+{
+    [SerializeField] private int coinTest;
+    private string CURRENCY_SAVE_KEY = "MYGAME_CURRENCY";
+    
+    public int TotalCoins { get; set; }
+
+    private void Start()
+    {
+        PlayerPrefs.DeleteKey(CURRENCY_SAVE_KEY);
+        LoadCoins();
+    }
+
+    private void LoadCoins()
+    {
+        TotalCoins = PlayerPrefs.GetInt(CURRENCY_SAVE_KEY, coinTest);
+    }
+    
+    public void AddCoins(int amount)
+    {
+        TotalCoins += amount;
+        PlayerPrefs.SetInt(CURRENCY_SAVE_KEY, TotalCoins);
+        PlayerPrefs.Save();
+    }
+
+    public void RemoveCoins(int amount)
+    {
+        if (TotalCoins >= amount)
+        {
+            TotalCoins -= amount;
+            PlayerPrefs.SetInt(CURRENCY_SAVE_KEY, TotalCoins);
+            PlayerPrefs.Save();
+        }
+    }
+    /*
+    private void AddCoins(Enemy enemy)
+    {
+        AddCoins(enemy.DeathCoinReward);
+    }
+    
+    private void OnEnable()
+    {
+        EnemyHealth.OnEnemyKilled += AddCoins;
+    }
+
+    private void OnDisable()
+    {
+        EnemyHealth.OnEnemyKilled -= AddCoins;
+    }*/
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject newInstance = new();
+                    instance = newInstance.AddComponent<T>();
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    public void Awake()
+    {
+        instance = this as T;
+    }
+}
