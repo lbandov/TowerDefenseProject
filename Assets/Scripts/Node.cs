8ad6376849760ea61053860f4ecd086ee21df23a
sbandov <stoyan.bandov@hotmail.com>
2022-06-23 19:32:04 +0300

    Node and UIManager

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public static Action<Node> OnNodeSelected;
+    public static Action OnTurretSold;
+
+    [SerializeField] private GameObject attackRangeSprite;
+    
+    public Turret Turret { get; set; }
+
+    private float _rangeSize;
+    private Vector3 _rangeOriginalSize;
+
+    private void Start()
+    {
+        _rangeSize = attackRangeSprite.GetComponent<SpriteRenderer>().bounds.size.y;
+        _rangeOriginalSize = attackRangeSprite.transform.localScale;
+    }
+    public void SetTurret(Turret turret)
+    {
+        Turret = turret;
+    }
+    public bool IsEmpty()
+    {
+        return Turret == null;
+    }
+    public void CloseAttackRangeSprite()
+    {
+        attackRangeSprite.SetActive(false);
+    }
+    public void SelectTurret()
+    {
+        OnNodeSelected?.Invoke(this);
+        if (!IsEmpty())
+        {
+            ShowTurretInfo();
+        }
+    }
+    public void SellTurret()
+    {
+        if (!IsEmpty())
+        {
+            CurrencySystem.Instance.AddCoins(Turret.TurretUpgrade.GetSellValue());
+            Destroy(Turret.gameObject);
+            Turret = null;
+            attackRangeSprite.SetActive(false);
+            OnTurretSold?.Invoke();
+        }
+    }
+
+    private void ShowTurretInfo()
+    {
+        attackRangeSprite.SetActive(true);
+        attackRangeSprite.transform.localScale = _rangeOriginalSize * Turret.AttackRange / (_rangeSize / 2);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : Singleton<UIManager>
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject turretShopPanel;
+    [SerializeField] private GameObject nodeUIPanel;
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private GameObject winPanel;
+
+    [Header("Text")] 
+    [SerializeField] private TextMeshProUGUI upgradeText;
+    [SerializeField] private TextMeshProUGUI sellText;
+    [SerializeField] private TextMeshProUGUI turretLevelText;
+    [SerializeField] private TextMeshProUGUI gameOverTotalCoinsText;
+    
+    private Node _currentNodeSelected;
+
+
+
+    public void SlowTime()
+    {
+        Time.timeScale = 0.5f;
+    }
+
+    public void ResumeTime()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void FastTime()
+    {
+        Time.timeScale = 2f;
+    }
+
+    public void PauseTime()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ShowGameOverPanel()
+    {
+        gameOverPanel.SetActive(true);
+        gameOverTotalCoinsText.text = CurrencySystem.Instance.TotalCoins.ToString();
+    }
+
+    public void ShowWinPanel()
+    {
+        winPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+
+    
+    public void CloseTurretShopPanel()
+    {
+        turretShopPanel.SetActive(false);
+    }
+
+    public void CloseNodeUIPanel()
+    {
+        _currentNodeSelected.CloseAttackRangeSprite();
+        nodeUIPanel.SetActive(false);
+    }
+    
+    public void UpgradeTurret()
+    {
+        _currentNodeSelected.Turret.TurretUpgrade.UpgradeTurret();
+        UpdateUpgradeText();
+        UpdateTurretLevel();
+        UpdateSellValue();
+    }
+
+    public void SellTurret()
+    {
+        _currentNodeSelected.SellTurret();
+        _currentNodeSelected = null;
+        nodeUIPanel.SetActive(false);
+    }
+    
+    private void ShowNodeUI()
+    {
+        nodeUIPanel.SetActive(true);
+        UpdateUpgradeText();
+        UpdateTurretLevel();
+        UpdateSellValue();
+    }
+
+    private void UpdateUpgradeText()
+    {
+        upgradeText.text = _currentNodeSelected.Turret.TurretUpgrade.UpgradeCost.ToString();
+    }
+
+    private void UpdateTurretLevel()
+    {
+        turretLevelText.text = $"Level {_currentNodeSelected.Turret.TurretUpgrade.Level}";
+    }
+
+    private void UpdateSellValue()
+    {
+        int sellAmount = _currentNodeSelected.Turret.TurretUpgrade.GetSellValue();
+        sellText.text = sellAmount.ToString();
+    }
+    
+    private void NodeSelected(Node nodeSelected)
+    {
+        _currentNodeSelected = nodeSelected;
+        if (_currentNodeSelected.IsEmpty())
+        {
+            turretShopPanel.SetActive(true);
+        }
+        else
+        {
+            ShowNodeUI();
+        }
+    }
+    
+    private void OnEnable()
+    {
+        Node.OnNodeSelected += NodeSelected;
+    }
+
+    private void OnDisable()
+    {
+        Node.OnNodeSelected -= NodeSelected;
+    }
+}
