344861e721bc75cf8df16555c211208444812882
david.cabaniuk <david.cabaniuk@cloudcall.com>
2022-06-23 23:45:39 +0100

    level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    [SerializeField] private int lives = 10;
+
+    public int TotalLives { get; set; }
+    public int CurrentWave { get; set; }
+
+    private void Start()
+    {
+        TotalLives = lives;
+        CurrentWave = 1;
+    }
+
+    private void ReduceLives(Enemy enemy)
+    {
+        TotalLives--;
+        if (TotalLives <= 0)
+        {
+            TotalLives = 0;
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+    }
+
+    private void WaveCompleted()
+    {
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEndReached += ReduceLives;
+    }
+
+    private void OnDisable()
+    {
+        Enemy.OnEndReached -= ReduceLives;
+    }
+}
